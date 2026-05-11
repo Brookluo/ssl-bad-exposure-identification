8b238e434a98aab6a5adf430620f7d34559a8fff
Yufeng Luo <brookluo34@hotmail.com>
2026-05-11 00:51:04 -0700

    docs: update README and SLURM scripts for decam-qa CLI

--- src/run-dist-job.sh
+++ src/run-dist-job.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -eux
-
 module load pytorch/2.6.0
-
-node_id=node0 #${SLURM_NODEID}
+node_id=node0
 output_dir=/pscratch/sd/b/brookluo/decam-exposure/revision/${node_id}
 [ ! -d $output_dir ] && mkdir $output_dir
-
 src_dir=$HOME/ssl-exposure-identification-paper/src
 export PYTHONPATH=$src_dir:$PYTHONPATH
-# cd $src_dir
-
-python $src_dir/master_eval.py -dir ${output_dir} \
-                                --dset-path /pscratch/sd/b/brookluo/decam-exposure/revision/proc-data/${node_id}_dr10_sample.csv \
-                                --dr dr10 \
-                                --cont
-
+python -m decam_qa.cli embed \
+    --config $src_dir/../configs/embed.yaml \
+    --dset /pscratch/sd/b/brookluo/decam-exposure/revision/proc-data/${node_id}_dr10_sample.csv \
+    --dr dr10 \
+    --cont
